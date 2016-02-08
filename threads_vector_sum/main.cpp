5115f9007d1c1c1de726f6ddca5121079755c41c
Georgy Schukin <georgy.schukin@gmail.com>
2016-02-08 21:19:27 +0600

    Update vector sum example

--- threads_vector_sum/main.cpp
+++ threads_vector_sum/main.cpp
@@ -14,15 +14,6 @@
 const int NUM_OF_THREADS_DEFAULT = std::thread::hardware_concurrency();
 const int VECTOR_SIZE_DEFAULT = 10000;
 
-template <class Iterator, class Value>
-void thread_func(Iterator start, Iterator end, Value *sum) {
-    Value local_sum = 0;
-    for (auto it = start; it != end; it++) {
-        local_sum += (*it);
-    }
-    *sum += local_sum; // sum into shared var
-}
-
 std::vector<int> get_chunk_sizes(int size, int num_of_chunks) {
     std::vector<int> chunk_sizes(num_of_chunks);
     const int chunk_size = size / num_of_chunks;
@@ -33,24 +24,29 @@ std::vector<int> get_chunk_sizes(int size, int num_of_chunks) {
     return chunk_sizes;
 }
 
-int main(int argc, char **argv) {
-    int num_of_threads = (argc > 1 ? std::stoi(argv[1]) : NUM_OF_THREADS_DEFAULT);
-    int vector_size = (argc > 2 ? std::atoi(argv[2]) : VECTOR_SIZE_DEFAULT);
+using Value = int;
+using Vector = std::vector<Value>;
 
-    using Value = int;
-    using Vector = std::vector<Value>;
-
-    Vector data(vector_size);
-
-    for (int i = 0; i < vector_size; i++) {
-        data[i] = i;
+void init_vector(Vector &data) {
+    Value value = 0;
+    for (auto &elem: data) {
+        elem = value++;
     }
+}
 
+template <class Iterator, class ValueT>
+void thread_func(Iterator start, Iterator end, ValueT *sum) {
+    ValueT local_sum = 0;
+    for (auto it = start; it != end; it++) {
+        local_sum += (*it);
+    }
+    *sum += local_sum; // sum into shared var
+}
+
+Value compute_vector_sum(Vector &data, int num_of_threads) {
     std::vector<std::thread> threads;
-    std::vector<int> chunk_sizes = get_chunk_sizes(vector_size, num_of_threads);
+    std::vector<int> chunk_sizes = get_chunk_sizes(data.size(), num_of_threads);
     Value sum = 0;
-
-    Timer timer;
 
     auto start = data.begin();
     for (int i = 0; i < num_of_threads; i++) {
@@ -64,9 +60,26 @@ int main(int argc, char **argv) {
         thread.join();
     }
 
+    return sum;
+}
+
+int main(int argc, char **argv) {
+    int num_of_threads = (argc > 1 ? std::stoi(argv[1]) : NUM_OF_THREADS_DEFAULT);
+    int vector_size = (argc > 2 ? std::atoi(argv[2]) : VECTOR_SIZE_DEFAULT);
+
+    Vector data(vector_size);   
+
+    init_vector(data);
+
+    Timer timer;
+
+    Value sum = compute_vector_sum(data, num_of_threads);
+
     auto time = timer.elapsed();
-    std::cout << "Wall time is " << std::fixed << std::setprecision(5) << time << " seconds" << std::endl;
-    std::cout << "Vector sum is " << sum << std::endl;
+
+    std::cout << "Num of threads: " << num_of_threads << ", vector size: " << vector_size << std::endl;
+    std::cout << "Wall time: " << std::fixed << std::setprecision(5) << time << " seconds" << std::endl;
+    std::cout << "Vector sum: " << sum << std::endl;
 
     return 0;
 }
