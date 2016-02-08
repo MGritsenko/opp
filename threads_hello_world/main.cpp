e3ed175a5e8e31292a2b50f9700af25513135996
Georgy Schukin <georgy.schukin@gmail.com>
2016-02-08 12:58:36 +0600

    Add thread Hello World example

---
+++ threads_hello_world/main.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <thread>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+void thread_func(int thread_id) {
+    std::ostringstream out;
+    out << "Hello, I am thread #" << thread_id << "\n";
+    std::cout << out.str();
+}
+
+int main(int argc, char **argv) {
+    int num_of_threads = (argc > 1 ? std::stoi(argv[1]) : 4);
+
+    std::vector<std::thread> threads;
+    for (int i = 0; i < num_of_threads; i++) {
+        threads.push_back(std::thread(thread_func, i));
+    }
+    for (auto &thread : threads) {
+        thread.join();
+    }
+    return 0;
+}
